1a44f19bdb3c1fe5f9a0a0280e37a83ddaa8c5de
Andrii Ponomarov <andrii.ponomarov@gmail.com>
2025-01-21 14:03:03 -0500

    Initial commit

---
+++ content/_meta.js
@@ -0,0 +1,13 @@
+export default {
+  index: 'Overview',
+  support: {
+    title: 'Support',
+    type: 'page',
+    href: 'https://support.sheeter.sh'
+  },
+  trial: {
+    title: 'Start a free trial',
+    type: 'page',
+    href: 'https://marketplace.atlassian.com'
+  },
+}
